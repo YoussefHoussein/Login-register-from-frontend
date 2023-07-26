f6a523daec0defdcd62cb2ac21ca09b01d604101
unknown <youssefhoussein701@gmail.com>
2023-07-26 21:33:11 +0300

    fix the register

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -43,10 +43,64 @@ pages.page_index =  () => {
 
 pages.page_register = () => {
     const articles_url = pages.base_url + "register.php";
+    const first_name = document.getElementById('first_name')
+    const last_name =document.getElementById('last_name')
+    const username=document.getElementById('username')
+    const password =document.getElementById('password')
+    const confirm =document.getElementById('confirm')
+    const btn = document.getElementById('login')
+    const alert = document.getElementById('alert')
+    const alert_text = document.getElementById('alert-text')
+    btn.addEventListener('click', function(){
+        if(first_name.value == "" || last_name.value == "" || username.value == "" || password.value =="" || confirm.value == ""){
+            alert_text.innerHTML = "Please fill all the fields"
+            alert.style.display = "block"
+            alert.addEventListener('click',function(){
+                alert.style.display = "none"
+            })
+        }
+        else{
+            if(password.value != confirm.value){
+                alert_text.innerHTML =  "Please Confirm the password correctly"
+                alert.style.display = "block"
+                alert.addEventListener('click',function(){
+                    alert.style.display = "none"
+                })
+            }
+            else{
+                const data = new FormData()
+                data.append("first_name",first_name.value)
+                data.append("last_name",last_name.value)
+                data.append("username",username.value)
+                data.append("password",password.value)
+
+                fetch("http://localhost/Login-register-backend/register.php" , {
+                    method: "POST",
+                    body: data
+                })
+                .then(response => response.json())
+                .then(newPost => {
+                  if(newPost.status == "success"){
+                    localStorage.setItem('firstname',first_name.value)
+                    localStorage.setItem('lastname',last_name.value)
+                    document.getElementById('linking').click()
+                  }
+                })
+                .catch(error => console.log(error))
+            }
+        }
+    })
     
 }
 
+pages.page_result = () => {
+    const first_name = localStorage.getItem('firstname')
+    const last_name = localStorage.getItem('lastname')
+    const result = document.getElementById('result')
 
+    result.innerHTML += first_name+" "+last_name
+}
 pages.loadFor = (page) => {
     eval("pages.page_" + page + "();")
 }
+
